7f374e13844f7ecb8ed3f4de1dc07a59a75cad71
nitekode <magnus@nitekode.com>
2026-04-26 14:43:42 +0200

    Add a new example demonstrating how to use groups

---
+++ examples/math/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/nitekode/cli"
+)
+
+func main() {
+	cli.Name("Math")
+	cli.Version("1.0")
+
+	cli.Command("pi", func() error {
+		fmt.Fprintln(cli.Out(), "3.14159")
+		return nil
+	})
+
+	cli.Group("calc", func(ga cli.GroupAdder) {
+		ga.Command("add {a} {b}", addHandler)
+		ga.Command("sub {a} {b}", subHandler)
+	})
+
+	cli.Run()
+}
+
+func addHandler(a string, b string) error {
+	numA, err := strconv.Atoi(a)
+	if err != nil {
+		return fmt.Errorf("could not convert %s to an int", a)
+	}
+	numB, err := strconv.Atoi(b)
+	if err != nil {
+		return fmt.Errorf("could not convert %s to an int", b)
+	}
+
+	fmt.Fprintf(cli.Out(), "%d + %d = %d\n", numA, numB, numA+numB)
+
+	return nil
+
+}
+
+func subHandler(a string, b string) error {
+	numA, err := strconv.Atoi(a)
+	if err != nil {
+		return fmt.Errorf("could not convert %s to an int", a)
+	}
+	numB, err := strconv.Atoi(b)
+	if err != nil {
+		return fmt.Errorf("could not convert %s to an int", b)
+	}
+
+	fmt.Fprintf(cli.Out(), "%d - %d = %d\n", numA, numB, numA-numB)
+
+	return nil
+
+}
