cffcf07f4f1475d5caaf2d13e2523f7bf67ee337
nitekode <magnus@nitekode.com>
2026-05-03 20:27:56 +0200

    Example demonstraing the usage of flags

---
+++ examples/text/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/nitekode/cli"
+)
+
+type globalFlags struct {
+	Repeat int `default:"1"`
+}
+
+type formatterFlags struct {
+	globalFlags
+	Prefix string
+}
+
+type upperFlags struct {
+	formatterFlags
+	Strong bool
+}
+
+func main() {
+	cli.GlobalFlags(globalFlags{})
+	cli.Group("format", "Format a text string", formatGroup, cli.Flags(formatterFlags{}))
+	cli.Run()
+}
+
+func formatGroup(ga cli.GroupAdder) {
+	ga.Command("upper {text}", "Uppercase text", upperHandler,
+		cli.Flags(upperFlags{}),
+	)
+	ga.Command("lower {text}", "Lowercase text", lowerHandler,
+		cli.Flags(upperFlags{}),
+	)
+}
+
+func upperHandler(flags upperFlags, text string) error {
+	for i := 0; i < flags.globalFlags.Repeat; i++ {
+		cli.Out().Write([]byte(transform(text, strings.ToUpper, flags.Prefix, flags.Strong)))
+	}
+
+	return nil
+}
+
+func lowerHandler(flags upperFlags, text string) error {
+	for i := 0; i < flags.globalFlags.Repeat; i++ {
+		cli.Out().Write([]byte(transform(text, strings.ToLower, flags.Prefix, flags.Strong)))
+	}
+
+	return nil
+}
+
+func transform(s string, t func(string) string, prefix string, strong bool) string {
+	b := bytes.Buffer{}
+
+	fmt.Fprintf(&b, "%s%s", prefix, t(s))
+	if strong {
+		fmt.Fprint(&b, "!")
+	}
+	fmt.Fprintln(&b)
+
+	return b.String()
+}
